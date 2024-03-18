27f779b0111cfac1ddb4e3b841044a9fabd3ef77
vd <33198864+vd2org@users.noreply.github.com>
2024-03-19 00:20:24 +0400

    Updated.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,9 +28,12 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-
 html_theme = 'sphinx_rtd_theme'
 html_logo = '_static/logo.png'
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
